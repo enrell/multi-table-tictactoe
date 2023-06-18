a2b10fd4553269098a5dd96687d25671a31f27ec
enrell <enrellsa10@gmail.com>
2023-06-18 02:46:48 -0300

    i dont no

---
+++ tictactoe/src/views/Game.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <div class="table">
+      
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.table {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 40vh;
+  height: 70vh;
+  background-color: white;
+}
+
+
+</style>
